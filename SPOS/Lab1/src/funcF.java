4042b01e3c5977e504aafbbabe2bd614d07e27af
Oleh Hubernator <50371410+o-huber@users.noreply.github.com>
2020-12-08 20:03:45 +0200

    change of structure

--- SPOS/Lab1/src/funcF.java
+++
@@ -1,8 +0,0 @@
-import spos.lab1.demo.IntOps;
-
-public class funcF {
-    public static void main() throws InterruptedException {
-        int x = IntOps.funcF(1);
-        System.out.println(x);
-    }
-}
